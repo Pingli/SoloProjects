45208a36edb31a5bcf3b3362b74ef1edd8e52197
Weikie <weikie_yeh@hotmail.com>
2016-06-08 17:04:21 +0200

    Player can shoot now

--- Isaac/UnityProject/Assets/Scripts/Entities/Player.cs
+++ Isaac/UnityProject/Assets/Scripts/Entities/Player.cs
@@ -3,14 +3,28 @@ using UnityEngine;
 
 public class Player : Entity
 {
+	[SerializeField]
+	private GameObject tearPrefab;
+
 	private void Start()
 	{
-		
+
 	}
-	
+
 	private void Update()
 	{
 		Movement();
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			SpawnTear();
+		}
+	}
+
+	private void SpawnTear()
+	{
+		GameObject obj = Instantiate(tearPrefab, transform.position + transform.forward, transform.rotation) as GameObject;
+		Tear tear = obj.GetComponent<Tear>();
+		tear.SetVelocity(GetVelocity());
 	}
 
 	private void Movement()
