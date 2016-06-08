14a71eed891a794126fa40bcd3a65837843d4fc7
Weikie <weikie_yeh@hotmail.com>
2016-06-08 16:32:32 +0200

    Basic enemy

--- Isaac/UnityProject/Assets/Scripts/Entities/Enemy.cs
+++ Isaac/UnityProject/Assets/Scripts/Entities/Enemy.cs
@@ -1,5 +1,25 @@
-﻿
+﻿using UnityEngine;
+
 public class Enemy : Entity
 {
+	public GameObject player;
 
+	private void Update()
+	{
+		ChaseTarget(player);
+	}
+
+	private void ChaseTarget(GameObject target)
+	{
+		MoveTo(target.transform.position);
+
+	}
+
+	private void MoveTo(Vector3 position)
+	{
+		Vector3 dir = position - transform.position;
+		float speed = 0.1f;
+		Vector3 vel = dir.normalized*speed;
+		transform.position += vel;
+	}
 }
