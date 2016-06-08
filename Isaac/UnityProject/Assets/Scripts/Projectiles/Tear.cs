76250fcf9528d8ebf6071a7aef9fcb85081248a1
Weikie <weikie_yeh@hotmail.com>
2016-06-08 17:03:54 +0200

    Added basic version of a projectile

---
+++ Isaac/UnityProject/Assets/Scripts/Projectiles/Tear.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tear : MonoBehaviour
+{
+	private Vector3 velocity;
+
+	[SerializeField]
+	private float gravityStrengthMin;
+	[SerializeField]
+	private float gravityStrengthMax;
+
+	private float currentGravityMod = 0;
+
+	private void FixedUpdate()
+	{
+		transform.position += velocity;
+		velocity = new Vector3(velocity.x, velocity.y - currentGravityMod, velocity.z);
+	}
+
+	public void SetVelocity(Vector3 vel)
+	{
+		velocity = vel;
+	}
+
+	private void OnCollisionEnter(Collision other)
+	{
+		if (other.transform.name == "Floor")
+		{
+			Destroy(gameObject);
+		}
+	}
+
+}
