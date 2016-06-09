17581fb5392fca1e3aa83bef8eb9653e37402ab6
Weikie <weikie_yeh@hotmail.com>
2016-06-09 15:01:05 +0200

    Swapped out GLFW with SFML instead

--- pacman/src/main.cpp
+++ pacman/src/main.cpp
@@ -1,60 +1,26 @@
-#include <GLFW/glfw3.h>
-#include <cstdio>
+#include <SFML/Graphics.hpp>
 
-void Update(GLFWwindow* window)
+int main()
 {
-	int state = glfwGetKey(window, GLFW_KEY_E);
-	if (state == GLFW_PRESS)
+	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+	sf::CircleShape shape(100.f);
+	shape.setFillColor(sf::Color::Green);
+
+	while (window.isOpen())
 	{
-		printf("pressed E\n");
-	}
-}
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+			{
+				window.close();
+			}
+		}
 
-void RenderStuff(GLFWwindow* window)
-{
-	glClearColor(1, 0, 0, 1);
-}
-
-int main(void)
-{
-	GLFWwindow* window;
-
-	/* Initialize the library */
-	if (!glfwInit())
-	{
-		return -1;
+		window.clear();
+		window.draw(shape);
+		window.display();
 	}
 
-	/* Create a windowed mode window and its OpenGL context */
-	window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
-	if (!window)
-	{
-		glfwTerminate();
-		return -1;
-	}
-
-	/* Make the window's context current */
-	glfwMakeContextCurrent(window);
-	//glfwSwapInterval();
-
-	printf(glfwGetVersionString());
-
-
-	/* Loop until the user closes the window */
-	while (!glfwWindowShouldClose(window))
-	{
-		/* Render here */
-		RenderStuff(window);
-
-		/* Swap front and back buffers */
-		glfwSwapBuffers(window);
-
-		/* Poll for and process events */
-		glfwPollEvents();
-
-		Update(window);
-	}
-
-	glfwTerminate();
 	return 0;
 }
