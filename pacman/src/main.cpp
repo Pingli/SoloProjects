83939dcbc9de69a7fa94afc411f3cf3adec3596a
Weikie <weikie_yeh@hotmail.com>
2016-06-09 13:11:35 +0200

    Added visual studio project with GLFW linked

---
+++ pacman/src/main.cpp
@@ -0,0 +1,64 @@
+
+
+#include <GLFW/glfw3.h>
+//#include <GLFW/glfw3native.h>
+//#include <gl/GL.h>
+#include <cstdio>
+
+void Update(GLFWwindow* window)
+{
+	int state = glfwGetKey(window, GLFW_KEY_E);
+	if (state == GLFW_PRESS)
+	{
+		printf("pressed E\n");
+	}
+}
+
+void RenderStuff(GLFWwindow* window)
+{
+	glClearColor(0, 0, 0, 1);
+}
+
+int main(void)
+{
+	GLFWwindow* window;
+
+	/* Initialize the library */
+	if (!glfwInit())
+	{
+		return -1;
+	}
+
+	/* Create a windowed mode window and its OpenGL context */
+	window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+	if (!window)
+	{
+		glfwTerminate();
+		return -1;
+	}
+
+	/* Make the window's context current */
+	glfwMakeContextCurrent(window);
+	//glfwSwapInterval();
+
+
+
+	/* Loop until the user closes the window */
+	while (!glfwWindowShouldClose(window))
+	{
+		/* Render here */
+		//RenderStuff(window);
+		glClearColor(0, 0, 0, 1);
+
+		/* Swap front and back buffers */
+		glfwSwapBuffers(window);
+
+		/* Poll for and process events */
+		glfwPollEvents();
+
+		Update(window);
+	}
+
+	glfwTerminate();
+	return 0;
+}
