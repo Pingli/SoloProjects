f12684dce810c7ab5dd9d009bcb58a9e5010c1cf
Weikie <weikie_yeh@hotmail.com>
2016-06-15 13:48:48 +0200

    Added more globals

--- pacman/src/Settings.cpp
+++ pacman/src/Settings.cpp
@@ -1,8 +1,18 @@
 #include "Settings.hpp"
 
-extern const int SCREEN_WIDTH = 500;
-extern const int SCREEN_HEIGHT = 500;
+
+extern const int LEVEL_WIDTH_COUNT = 28;
+extern const int LEVEL_HEIGHT_COUNT = 31;
+extern const int SCREEN_WIDTH = TILE_WIDTH * LEVEL_WIDTH_COUNT;
+extern const int SCREEN_HEIGHT = TILE_HEIGHT * LEVEL_HEIGHT_COUNT;
+extern const int TILE_WIDTH = 6;
+extern const int TILE_HEIGHT = 6;
 extern const char* WINDOW_TITLE = "Pacman";
 extern const std::string ASSET_PATH = "../assets/";
 extern const std::string LEVEL_FILE_NAME = "level.csv";
 extern const std::string LEVEL_FULL_PATH = ASSET_PATH + LEVEL_FILE_NAME;
+extern const std::string SPRITESHEET_PACMAN = "spritesheet.png";
+extern const std::string SPRITESHEET_PACMAN_FULL_PATH = ASSET_PATH + SPRITESHEET_PACMAN;
+
+
+
--- pacman/src/Settings.hpp
+++ pacman/src/Settings.hpp
@@ -1,12 +1,26 @@
 #pragma once
 #include <string>
 
+extern const int LEVEL_WIDTH_COUNT;
+extern const int LEVEL_HEIGHT_COUNT;
 extern const int SCREEN_WIDTH;
 extern const int SCREEN_HEIGHT;
+extern const int TILE_WIDTH;
+extern const int TILE_HEIGHT;
 extern const char* WINDOW_TITLE;
 extern const std::string ASSET_PATH;
 extern const std::string LEVEL_FILE_NAME;
 extern const std::string LEVEL_FULL_PATH;
+extern const std::string SPRITESHEET_PACMAN;
+extern const std::string SPRITESHEET_PACMAN_FULL_PATH;
 
-
-
+enum class Tile
+{
+	PICKUP = 16,
+	EMPTY = 160,
+	PACMAN,
+	BLINKY,
+	PINKY,
+	INKY,
+	CLIDE
+};
