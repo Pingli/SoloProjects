1b2548a2703a8638f2fffc5f25e811cda884a096
Weikie <weikie_yeh@hotmail.com>
2016-06-15 13:51:20 +0200

    Added level loading

--- pacman/src/Game/LevelLoader.cpp
+++ pacman/src/Game/LevelLoader.cpp
@@ -3,7 +3,8 @@
 #include <string>
 #include <vector>
 #include <sstream>
-
+#include "../Settings.hpp"
+#include "../Entity2D.hpp"
 
 
 std::vector<std::vector<std::string>> LevelLoader::LoadLevel(const std::string& filePath)
@@ -11,20 +12,45 @@ std::vector<std::vector<std::string>> LevelLoader::LoadLevel(const std::string& 
 	std::vector<std::vector<std::string>> level;
 	FileTo2DVector(filePath, level);
 
+	SpawnLevelObjects(level);
+
+	return level;
+}
+
+void LevelLoader::SpawnLevelObjects(const std::vector<std::vector<std::string>>& level)
+{
+	int i = 0;
 	for (auto vectorIterator = level.begin(); vectorIterator != level.end(); ++vectorIterator)
 	{
+		int n = 0;
 		auto rowVector = *vectorIterator;
 		for (auto it = rowVector.begin(); it != rowVector.end(); ++it)
 		{
-			std::string value = *it;
-			static int count = 0;
-			printf("%i \n", ++count);
+			//maybe not that safe?
+			int tileNumber = atoi((*it).c_str());
+			Tile tileEnum = (Tile)tileNumber;
+			//static int count = 0;
+			//intentional memory leak at this point, needs to be moved somewhere else
+			Entity2D* ent;
 
+			switch (tileEnum)
+			{
+				case Tile::EMPTY:
+				case Tile::PICKUP:
+				default:
+					ent = new Entity2D();
+					ent->SetTextureFromSpritesheet(SPRITESHEET_PACMAN_FULL_PATH, tileNumber);
+					ent->SetPosition(n * TILE_WIDTH, i * TILE_HEIGHT);
+					break;
 
+					break;
+					//printf("%i is not yet supported, should add it asap.\n", tileEnum);
+					break;
+			}
+			++n;
 		}
+		++i;
 	}
-
-	return level;
 }
 
 void LevelLoader::FileTo2DVector(const std::string& filePath, std::vector<std::vector<std::string>>& level)
--- pacman/src/Game/LevelLoader.hpp
+++ pacman/src/Game/LevelLoader.hpp
@@ -6,8 +6,9 @@ class LevelLoader
 {
 	public:
 		static std::vector<std::vector<std::string>> LoadLevel(const std::string& filePath);
-		static void FileTo2DVector(const std::string& filePath, std::vector<std::vector<std::string>>& level);
 	private:
+		static void FileTo2DVector(const std::string& filePath, std::vector<std::vector<std::string>>& level);
+		static void SpawnLevelObjects(const std::vector<std::vector<std::string>>& level);
 		static std::vector<std::string> Split(const std::string& s, char delim);
 		static std::vector<std::string>& Split(const std::string& s, char delim, std::vector<std::string>& elems);
 };
